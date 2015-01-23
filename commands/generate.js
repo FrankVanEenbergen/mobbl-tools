69ec3455d0fe710fb2cea6cda18d2dd78f98762f
Pjotter Tommassen <p.tommassen@gmail.com>
2015-01-23 13:52:53 +0100

    - document generator is working \O/

--- commands/generate.js
+++ commands/generate.js
@@ -5,6 +5,7 @@ var program = require ('commander');
 var fs = require('fs');
 var json2mobbl = require('../lib/json2mobbl/json2mobbl');
 var mobbl = require('../lib/mobbl-common.js');
+var util = require('util');
 
 GenerateCommand.run = function(args) {
   var self = this;
@@ -13,9 +14,12 @@ GenerateCommand.run = function(args) {
   program
   .command('docdef <name>')
   .description('Generate a document definition')
-  .option('-f, --from <file>', 'Use a file to base the document on. Supported types: json')
+  .option('-f, --from [file]', 'Use a file to base the document on. Supported types: json')
+  .option('-d, --datahandler [name]', 'Use name as the datahandler. Default: MBMemoryDataHandler')
+  .option('-n, --noautocreate', 'Sets autocreate to false')
   .action(function (name, options) {
-    self.generateDocDef (name, options.from);
+    debugger;
+    self.generateDocDef (name, options);
   });
 
   program.parse (args);
@@ -23,18 +27,22 @@ GenerateCommand.run = function(args) {
 
 }
 
-GenerateCommand.generateDocDef = function (name, input) {
-  if (input) {
+GenerateCommand.generateDocDef = function (name, options) {
+  var preamble='<Configuration xmlns="http://itude.com/schemas/MB/1.0">\n\t<Documents>\n\t\t<Document name="%s" dataManager="%s" autoCreate="%s">\n';
+  preamble = util.format (preamble, name, options.datahandler?options.datahandler : 'MBMemoryDataHandler', options.noautocreate?'false' : 'true');
 
-  fs.readFile (input, 'utf8', function (err, data) {
-    if (err) return console.log("Error: " + err);
-    debugger;
+  var postamble = '\t\t</Document>\n\t</Documents>\n</Configuration>';
 
-    var object = JSON.parse(data);
-    var out = fs.createWriteStream (mobbl.config.documentPath + "/" + name);
-    json2mobbl (object, out);
-  });
-}
+  var out = fs.createWriteStream (mobbl.config.docdefPath + "/" + name + ".xml");
+  out.write (preamble);
+
+  if (options.from) {
+    var data = fs.readFileSync (options.from);
+    var object = JSON.parse(data.toString ());
+    json2mobbl (object, out, 3);
+  }
+
+  out.write (postamble);
 }
 
 GenerateCommand.description = 'Generate command!';
